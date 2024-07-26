05ae9be5ddacbe20bfac769bf0c3780694d917ad
Neil Chauhan <neilchauhan2@gmail.com>
2024-07-27 01:21:50 +0530

    style: update template card icon<>header layout

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -3,8 +3,10 @@ import { TemplateProps } from '@/app/(categoryTemplate)/templates'
 const TemplateCard = (template: TemplateProps) => {
   return (
     <div className='p-5 shadow-md rounded-md border bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all'>
-      <template.icon className='w-8 h-8' />
-      <h2 className='font-medium text-lg'>{template.name}</h2>
+      <div className='flex items-center gap-3'>
+        <template.icon className='w-8 h-8' />
+        <h2 className='font-medium text-lg'>{template.name}</h2>
+      </div>
       <p className='text-gray-500 line-clamp-3'>{template.desc}</p>
     </div>
   )
