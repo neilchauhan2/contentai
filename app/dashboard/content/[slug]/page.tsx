51215d45e4853698e775c1aac5c76f166c74f53e
Neil Chauhan <neilchauhan2@gmail.com>
2024-07-29 17:59:42 +0530

    feat: Add DB insertion method

--- app/dashboard/content/[slug]/page.tsx
+++ app/dashboard/content/[slug]/page.tsx
@@ -7,6 +7,10 @@ import Link from 'next/link'
 import { MoveLeft } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { chatSession } from '@/app/utils/GeminiModel'
+import { db } from '@/db'
+import { Output } from '@/db/schema'
+import { useUser } from '@clerk/nextjs'
+import moment from 'moment'
 
 interface CreateContentPageProps {
   params: {
@@ -18,14 +22,39 @@ function CreateContentPage({ params: { slug } }: CreateContentPageProps) {
   const selectedTemplate = templates.find(template => template.slug === slug)
   const [loading, setLoading] = useState(false);
   const [output, setOutput] = useState('');
+  const { user } = useUser();
+
+  const insertIntoDB = async (formData: Record<string, string>, slug: string, response: string) => {
+    try {
+      const result = await db.insert(Output).values({
+        formData: JSON.stringify(formData),
+        slug,
+        response,
+        createdBy: user?.primaryEmailAddress?.emailAddress || 'unknown',
+        createdAt: moment().format('DD/MM/YYYY'),
+      });
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 
   const generateAIContent = async (formData: Record<string, string>) => {
-    setLoading(true);
-    const prompt = selectedTemplate?.aiPrompt;
-    const preparedPrompt = JSON.stringify(formData) + ", " + prompt;
-    const result = await chatSession.sendMessage(preparedPrompt);
-    setOutput(result.response.text());
-    setLoading(false);
+    try {
+      if (selectedTemplate) {
+        setLoading(true);
+        const prompt = selectedTemplate?.aiPrompt;
+        const preparedPrompt = JSON.stringify(formData) + ", " + prompt;
+        const result = await chatSession.sendMessage(preparedPrompt);
+        setOutput(result.response.text());
+        await insertIntoDB(formData, selectedTemplate.slug, result.response.text());
+        setLoading(false);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
 
   return (
